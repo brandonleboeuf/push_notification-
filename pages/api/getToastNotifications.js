5bd006bb32511ca4dee4dee631438e3c5995bd73
Brandon LeBoeuf <BrandonL@henry-v.com>
2022-01-19 10:41:14 -0800

    updates times for codesandbox

--- pages/api/getToastNotifications.js
+++ pages/api/getToastNotifications.js
@@ -2,19 +2,19 @@
 const data = [
   {
     id: 1, // Id used to ensure that message isn't displayed multiple times
-    scheduledTime: new Date('January 19, 2022 08:45:00').getTime(), // Time for the message to be sent to the App
+    scheduledTime: new Date('January 19, 2022 10:42:00').getTime(), // Time for the message to be sent to the App
     displayWindow: 60000 * 5, // If the user is logged in any time within one minuit of scheduledTime, this will be shown.
     message: "This is the message for nicole"
   },
   {
     id: 2,
-    scheduledTime: new Date('January 19, 2022 08:45:40').getTime(),
+    scheduledTime: new Date('January 19, 2022 10:42:40').getTime(),
     displayWindow: 60000 * 5,
     message: "Welco9me to the bigggg show BRODNOA"
   },
   {
     id: 3,
-    scheduledTime: new Date('January 19, 2022 08:46:00').getTime(),
+    scheduledTime: new Date('January 19, 2022 10:44:00').getTime(),
     displayWindow: 60000 * 5,
     message: "This is a long sentance that is kind of a run on. But you should check out the session that is coming soon baby, yua!!"
   },
